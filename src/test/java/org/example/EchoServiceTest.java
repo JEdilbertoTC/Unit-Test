bfafd07b9bdb02474efcf7051b32a958d8116f6a
JEdilbertoTC <joseedilberto367@outlook.com>
2020-11-08 19:52:10 -0600

    Mock

--- src/test/java/org/example/EchoServiceTest.java
+++ src/test/java/org/example/EchoServiceTest.java
@@ -1,47 +1,56 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.*;
 
 class EchoServiceTest {
     @Test
     public void givenValidRequestWhenEchoThenTrue_isResponse() throws IOException {
         //Given
         EchoService echoService = new EchoService();
-        String ip = "1.2";
-        int port = 9988;
-        String request = "HOLA MUNDO";
+        String request = "hola mundo";
+        byte [] messageinBytes = new byte[]{'h','o','l','a',' ', 'm','u','n','d','o'};
+        OutputStream outputStream = mock(OutputStream.class);
+        InputStream inputStream = mock(InputStream.class);
+        when(inputStream.readAllBytes()).thenReturn(messageinBytes);
+
         //When
-        TestOutStream outputStream = new TestOutStream();
-        TestInputStream inputStream = new TestInputStream(request);
-        boolean response = echoService.sendEchoMessage(request, outputStream, inputStream);
+        boolean response = echoService.sendEchoMessage(request,outputStream,inputStream);
 
         //Then
-        String messageSent = outputStream.getMessageSent();
-        assertEquals(request, messageSent);
+        verify(inputStream).readAllBytes();
+        verify(outputStream).write(messageinBytes);
+        verifyNoMoreInteractions(inputStream,outputStream);
         assertTrue(response);
     }
     @Test
     public void givenValidRequestAndWrongResponseWhenEchoThenTrue_isResponse() throws IOException {
         //Given
         EchoService echoService = new EchoService();
-        String ip = "1.2";
-        int port = 9988;
-        String request = "HOLA MUNDO";
+        String request = "hola mundo";
+        byte [] messageRequest = new byte[]{'h','o','l','a',' ', 'm','u','n','d','o'};
+        byte [] messageRespond = new byte[]{'h','o','l','a',' ', 'm','u','n','d','o','2'};
+
+        OutputStream outputStream = mock(OutputStream.class);
+        InputStream inputStream =  mock(InputStream.class);
+        when(inputStream.readAllBytes()).thenReturn(messageRespond);
+
         //When
-        TestOutStream outputStream = new TestOutStream();
-        TestInputStream inputStream = new TestInputStream("Otra Cosa");
         boolean response = echoService.sendEchoMessage(request, outputStream, inputStream);
 
         //Then
-        String messageSent = outputStream.getMessageSent();
-        assertEquals(request, messageSent);
+        verify(inputStream).readAllBytes();
+        verify(outputStream).write(messageRequest);
+        verifyNoMoreInteractions(inputStream, outputStream);
+
         assertFalse(response);
     }
 }
